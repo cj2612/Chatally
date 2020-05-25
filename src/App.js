4e376215fea0ff72668c11d5c23225d85acc7d38
cj2612 <37813129+cj2612@users.noreply.github.com>
2020-05-25 19:55:14 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import ChatallyLayout from './components/ChatallyLayout'
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+        <ChatallyLayout />
+    );
+  }
+}
+
+export default App;
