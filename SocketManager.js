f7e100da4cdbe36fbda8c69454951a96f5126af9
matnsk <65949187+matnsk@users.noreply.github.com>
2020-05-26 13:05:15 +0300

    SocketManager.js
    updated

---
+++ SocketManager.js
@@ -0,0 +1,132 @@
+const io = require('./index.js').io
+
+const { VERIFY_USER, USER_CONNECTED, USER_DISCONNECTED, 
+		LOGOUT, COMMUNITY_CHAT, MESSAGE_RECIEVED, MESSAGE_SENT,
+		TYPING, PRIVATE_MESSAGE  } = require('../Events')
+
+const { createUser, createMessage, createChat } = require('../Factories')
+
+let connectedUsers = { }
+
+let communityChat = createChat()
+
+
+
+module.exports = function(socket){
+					
+	// console.log('\x1bc'); //clears console
+	console.log("Socket Id:" + socket.id);
+
+	let sendMessageToChatFromUser;
+
+	let sendTypingFromUser;
+
+	//Verify Username
+	socket.on(VERIFY_USER, (nickname, callback)=>{
+		if(isUser(connectedUsers, nickname)){
+			callback({ isUser:true, user:null })
+		}else{
+			callback({ isUser:false, user:createUser({name:nickname, socketId:socket.id})})
+		}
+	})
+
+	//User Connects with username,first it verifies the user then it adds to the list
+	socket.on(USER_CONNECTED, (user)=>{
+		user.socketId = socket.id
+		connectedUsers = addUser(connectedUsers, user)
+		socket.user = user
+
+		sendMessageToChatFromUser = sendMessageToChat(user.name)
+		sendTypingFromUser = sendTypingToChat(user.name)
+
+		io.emit(USER_CONNECTED, connectedUsers)
+		console.log(connectedUsers);
+
+	})
+	
+	//User disconnects, removes the user from the list
+	socket.on('disconnect', ()=>{
+		if("user" in socket){
+			connectedUsers = removeUser(connectedUsers, socket.user.name)
+
+			io.emit(USER_DISCONNECTED, connectedUsers)
+			console.log("Disconnect", connectedUsers);
+		}
+	})
+
+
+	//User logsout
+	socket.on(LOGOUT, ()=>{
+		connectedUsers = removeUser(connectedUsers, socket.user.name)
+		io.emit(USER_DISCONNECTED, connectedUsers)
+		console.log("Disconnect", connectedUsers);
+
+	})
+
+	//Get Community Chat
+	socket.on(COMMUNITY_CHAT, (callback)=>{
+		callback(communityChat)
+	})
+
+	socket.on(MESSAGE_SENT, ({chatId, message})=>{
+		sendMessageToChatFromUser(chatId, message)
+	})
+
+	socket.on(TYPING, ({chatId, isTyping})=>{
+		sendTypingFromUser(chatId, isTyping)
+	})
+
+	socket.on(PRIVATE_MESSAGE, ({reciever, sender})=>{
+		if(reciever in connectedUsers){
+			const newChat = createChat({ name:`${reciever}&${sender}`, users:[reciever, sender] })
+			const recieverSocket = connectedUsers[reciever].socketId
+			socket.to(recieverSocket).emit(PRIVATE_MESSAGE, newChat)
+			socket.emit(PRIVATE_MESSAGE, newChat)
+		}
+	})
+
+}
+
+
+//Helper Functions
+// Returns a function that will take a chat id and a boolean isTyping and emits to the chatId if the other user is typing.
+
+function sendTypingToChat(user){
+	return (chatId, isTyping)=>{
+		io.emit(`${TYPING}-${chatId}`, {user, isTyping})
+	}
+}
+
+
+ //Returns a function that will take a chat id and message and emits to the chatId.
+
+function sendMessageToChat(sender){
+	return (chatId, message)=>{
+		io.emit(`${MESSAGE_RECIEVED}-${chatId}`, createMessage({message, sender}))
+	}
+}
+
+
+//Adds user to list with info passed in upon login.
+
+function addUser(userList, user){
+	let newList = Object.assign({}, userList)
+	newList[user.name] = user
+	return newList
+}
+
+
+//Removes user from the list in case of logout or dissconect.
+
+function removeUser(userList, username){
+	let newList = Object.assign({}, userList)
+	delete newList[username]
+	return newList
+}
+
+
+//Checks if the user is in list.
+
+function isUser(userList, username){
+  	return username in userList
+}
